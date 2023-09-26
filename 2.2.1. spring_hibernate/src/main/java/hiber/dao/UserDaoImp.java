6085e88782ed3bf3537e54b254fb49ac3fce6d0d
Vladimir <vm_77@list.ru>
2023-09-26 13:49:32 +0300

    commit5

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,18 +1,17 @@
 package hiber.dao;
 
 import hiber.model.User;
-
+import lombok.RequiredArgsConstructor;
 import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class UserDaoImp implements UserDao {
 
-    @Autowired
-    private SessionFactory sessionFactory;
+    private final SessionFactory sessionFactory;
 
     @Override
     public void add(User user) {
@@ -27,6 +26,7 @@ public class UserDaoImp implements UserDao {
         return query.getResultList();
     }
     @Override
+    @SuppressWarnings("unchecked")
     public List<User> findUserByCar(String model, int series) {
         TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery
         ("from User where car.model = :model and car.series = :series");
--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -2,32 +2,28 @@ package hiber.service;
 
 import hiber.dao.UserDao;
 import hiber.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class UserServiceImp implements UserService {
 
-
-   @Autowired
-   private UserDao userDao;
+   private final UserDao userDao;
 
    @Transactional
    @Override
    public void add(User user) {
-
       userDao.add(user);
    }
 
    @Transactional(readOnly = true)
    @Override
    public List<User> getlistUsers() {
-
       return userDao.getlistUsers();
    }
-
 
    @Transactional(readOnly = true)
    @Override
